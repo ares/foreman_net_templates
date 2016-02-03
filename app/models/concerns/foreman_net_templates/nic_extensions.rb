b19f548f27dca5b78f29aa6cc5ce09b239c8c076
Ewoud Kohl van Wijngaarden <ekohlvanwijngaarden@antagonist.nl>
2016-02-03 15:11:37 +0100

    Fix to_os_net_config

--- app/models/concerns/foreman_net_templates/nic_extensions.rb
+++ app/models/concerns/foreman_net_templates/nic_extensions.rb
@@ -3,28 +3,30 @@ module ForemanNetTemplates
     extend ActiveSupport::Concern
 
     def os_net_config_type
-      case interface_class(name).to_s
-        when 'Nic::Bond'
-          'linux_bond'
-        when 'Nic::Bridge'
-          'linux_bridge'
-        else
-          'interface'
-        end
+      case self.type
+      when 'Nic::Bond'
+        'linux_bond'
+      when 'Nic::Bridge'
+        'linux_bridge'
+      else
+        'interface'
+      end
     end
 
     def os_net_config_ipaddress
-      {:ip_netmask => "#{ip}/#{subnet.cidr}"}
+      [{:ip_netmask => "#{ip}/#{subnet.cidr}"}] if subnet
+      []
     end
 
     def to_os_net_config
-      os_net_config = Hash.new
-      os_net_config['name'] = identifier
-      os_net_config['type'] = os_net_config_type
-      os_net_config['addresses'] = Array.new(os_net_config_ipaddress)
-      #TODO: routes
-      #TODO: bond
-      #TODO: bridge
+      {
+        :name => identifier,
+        :type => os_net_config_type,
+        :addresses => os_net_config_ipaddress,
+        #TODO: routes
+        #TODO: bond
+        #TODO: bridge
+      }
     end
 
   end

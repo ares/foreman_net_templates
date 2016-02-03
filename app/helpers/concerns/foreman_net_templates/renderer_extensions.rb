ee0923b19e2e6bbc5a47ee338186b298b2f737eb
Marek Hulan <mhulan@redhat.com>
2016-02-03 22:55:52 +0100

    First working version!!

--- app/helpers/concerns/foreman_net_templates/renderer_extensions.rb
+++ app/helpers/concerns/foreman_net_templates/renderer_extensions.rb
@@ -4,7 +4,48 @@ module ForemanNetTemplates
 
     # create or overwrite instance methods...
     def build_network_config_files
-      'hello'
+      config_file = Tempfile.new(['foreman_net_template_config', '.json'])
+      config_file.write(@host.os_net_config_json)
+      config_file.close
+      result = `os-net-config --noop --config-file=#{config_file.to_path} --no-activate --provider #{get_provider(@host)}`
+      config_file.unlink
+
+      files = parse_os_net_config_result(result)
+      files.map { |path, content| wrap_in_cat(path, content.join("\n")) }.join("\n\n")
+    end
+
+    private
+
+    def get_provider(host)
+      case host.operatingsystem.family.downcase
+        when 'debian'
+          'eni'
+        else
+          'ifcfg'
+      end
+    end
+
+    def parse_os_net_config_result(result)
+      files = Hash.new { |h,k| h[k] = [] }
+      file = nil
+
+      result.split("\n").each do |line|
+        if (match_data = line.match(/File: (.+)/))
+          file = match_data[1]
+        elsif line == '----' || line.empty?
+          next
+        else
+          files[file] << line
+        end
+      end
+
+      files
+    end
+
+    def wrap_in_cat(target_path, content)
+      "cat > #{target_path} <<EOF
+#{content}
+EOF"
     end
   end
 end
--- app/models/concerns/foreman_net_templates/nic_extensions.rb
+++ app/models/concerns/foreman_net_templates/nic_extensions.rb
@@ -13,16 +13,18 @@ module ForemanNetTemplates
       end
     end
 
-    def os_net_config_ipaddress
-      [{:ip_netmask => "#{ip}/#{subnet.cidr}"}] if subnet
+    def os_net_config_ip_address
+      return [{:ip_netmask => "#{ip}/#{subnet.cidr}"}] if subnet
       []
     end
 
     def to_os_net_config
+      # TODO for debian a name is required but identifier is not required field
       {
         :name => identifier,
         :type => os_net_config_type,
-        :addresses => os_net_config_ipaddress,
+        :addresses => os_net_config_ip_address,
+        #TODO skip BMC
         #TODO: routes
         #TODO: bond
         #TODO: bridge
